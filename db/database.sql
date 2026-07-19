6b06cca1b69e462500a0ac5ad49380a5e2cfdfd1
Danielkwz0818 <danielkok5160@gmail.com>
2026-07-20 00:50:59 +0800

    done

--- db/database.sql
+++ db/database.sql
@@ -88,7 +88,7 @@ CREATE TABLE `orders` (
   `user_id` INT(11) NOT NULL,
   `order_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `total_amount` DECIMAL(10, 2) NOT NULL,
-  `status` ENUM('pending', 'completed', 'cancelled') NOT NULL DEFAULT 'pending',
+  `status` ENUM('pending', 'shipped', 'completed', 'cancelled') NOT NULL DEFAULT 'pending',
   `recipient_name` VARCHAR(100) NOT NULL,       
   `shipping_address` TEXT NOT NULL,             
   `phone_number` VARCHAR(20) NOT NULL,          
